d3ab8076ad9319912dab6038fb5f8fc42fe01a37
Alex Palacio <alexpalacio29@gmail.com>
2026-08-12 09:32:06 -0400

    fix: importar useMemo en App.tsx para resolver White Screen Error

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { Sidebar } from './components/layout/Sidebar';
 import { Toaster, toast } from 'react-hot-toast';
 import { Dashboard } from './components/Dashboard';
